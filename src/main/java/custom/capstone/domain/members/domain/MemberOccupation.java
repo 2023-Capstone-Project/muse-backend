e3107859d43cd2018b36aefb00e5201ec2685e16
leeseunghee00 <lsh9246@gmail.com>
2023-04-25 21:22:32 +0900

    refactor: 주석 삭제

--- src/main/java/custom/capstone/domain/members/domain/MemberOccupation.java
+++ src/main/java/custom/capstone/domain/members/domain/MemberOccupation.java
@@ -2,8 +2,4 @@ package custom.capstone.domain.members.domain;
 
 public enum MemberOccupation {
     NORMAL, DESIGNER    //일반인, 디자이너
-
-//    public static MemberOccupation toEnum(final String occupation) {
-//        return MemberOccupation.valueOf(occupation.toUpperCase());
-//    }
 }
