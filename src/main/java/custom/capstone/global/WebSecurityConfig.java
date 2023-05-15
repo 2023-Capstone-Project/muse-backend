9152f7fffaa0ccaf0a1e58332961231d0de12811
leeseunghee00 <lsh9246@gmail.com>
2023-05-15 20:03:30 +0900

    feat: spring security h2-console 권한 설정

---
+++ src/main/java/custom/capstone/global/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package custom.capstone.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests()
+//                .anyRequest()
+//                .permitAll();
+        http
+                .authorizeHttpRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
+    }
+}
